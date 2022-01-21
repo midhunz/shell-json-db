780b37107454ab2debdd393a488751913dfa5743
midhun pottammal <iris009147@gmail.com>
2022-01-21 16:53:06 +0400

    added index export

--- index.js
+++ index.js
@@ -1,22 +1,2 @@
-const Jdb = require('./Jdb');
-//create DB
-const db = new Jdb('db');
-//CAREATE cOLLECTIONS
-const users=db.createCollection('users');
-const comments=db.createCollection('comments');
-
-let user1 = {
-    name:'midhun',
-    country: 'india'
-}
-
-let user2 = {
-    name:'midhun',
-    country: 'india'
-}
-
-let a =[user1,user2];
-users.save(a);
-
-let u=users.find({});
-console.log(u);
+export { default as Jdb } from "./Jdb";
+export { default as Collection } from "./Collection";
